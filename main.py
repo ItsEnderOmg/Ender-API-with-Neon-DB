a6feef7eb6e0d9ac24671089748544dcbd7074b5
ItsEnderOmg <mezajostin38@gmail.com>
2026-06-23 14:43:08 -0600

    Created  a function to count users , and another one to delete a user by id

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from fastapi import FastAPI, HTTPException, Depends # Lo que usa la API
 from fastapi.middleware.cors import CORSMiddleware #N omas pa permitir ciertas solicitudes
 from database import Base, engine, SessionLocal
 from sqlalchemy.orm import Session
-from typing import Annotated, List
 import models
 import schemas
+
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
@@ -17,7 +17,7 @@ app.add_middleware(
     allow_methods=["*"],  # Permite todos los métodos (GET, POST, DELETE, etc)
     allow_headers=["*"],  # Permite todos los headers
 )
-
+#Esto es la coneccion practicamente
 def get_db(): 
     db = SessionLocal()
     try: 
@@ -25,21 +25,36 @@ def get_db():
     finally:
         db.close()
 
-# @app.get("/users/{user_id}")
-# async def get_users_by_id(user_id : int, db: Session = Depends(get_db)):
-#     #user = db.query(models.User).filter(schemas.User.id == user_id).first
-#     pass
-
-
 @app.get("/")
 async def root():
     return {"message": "Gyaru"}
+
+# Read User by id
+@app.get("/users/get_user_by_id/{id}", response_model = schemas.UserResponse)
+async def get_users_by_id(id : int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if user:
+        return user
+    raise HTTPException(status_code=404, details="User not found.")
+
+#Read all users
+@app.get("/users/get_all_users", response_model = list[schemas.UserResponse])
+async def get_all_users(db : Session = Depends(get_db)):
+    all_users = db.query(models.User).all()
+    if all_users:
+        return all_users
+    raise HTTPException(status_code=404, details="What?! There's nothing in the database...")
+
+#Count how many users are in the database
+@app.get("/users/count_users")
+async def count_users(db : Session = Depends(get_db)):
+    return db.query(models.User).count()
 
 # response_model hace que return muestre solo las instancias de ese objeto (de la base de datos)
 # que se llamen igual que las instancias de la clase que creamos en schemas
 @app.post("/users/create_user", response_model = schemas.UserResponse)
 # "user" sera un objeto de clase UserCreate (como lo creamos con BaseModel tiene validacion de datos)
-async def create_user(user : schemas.UserCreate ,db: Session = Depends(get_db)):
+async def create_user(user : schemas.UserCreate, db: Session = Depends(get_db)):
     # User es un objeto de una tabla creada en models
     new_user = models.User(
         # username,email,password son los nombres de las columnas en la tabla
@@ -58,14 +73,12 @@ async def create_user(user : schemas.UserCreate ,db: Session = Depends(get_db)):
     # Cuales valores deberiamos devolver 
     return {"id" : new_user.id, "username" : new_user.username, "email" : new_user.email}
 
-@app.get("/users/get_user_by_id/{id}", response_model = schemas.UserResponse)
-async def get_user(id : int, db : Session = Depends(get_db)):
+#Delete a user from the database
+@app.delete("/users/delete_user/{id}")
+async def delete_user(id : int, db : Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.id == id).first()
-    if user:
-        return user
-    raise HTTPException(status_code=404, details="ID doesn't exist")
-
-@app.get("/users/get_all_users")
-async def get_all_users(db : Session = Depends(get_db)):
-    result = db.query(models.User).all()
-    return result
+    if not user:
+        raise HTTPException(status_code=404, details="User ID not found")
+    db.delete(user)
+    db.commit()
+    return {"message": f"The user with id = {id} was deleted."} 
