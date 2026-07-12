5c2e372412ef46d15ee4b0c16624a30c976f5d23
ItzEnderOmg <mezajostin38@gmail.com>
2026-07-12 17:55:48 -0600

    Enabled the Type Checking Mode and set it to 'Standard', added some Type Narrowing for Pydantic in 'database.py'

--- database.py
+++ database.py
@@ -8,6 +8,11 @@ load_dotenv()
 #Le asigna el valor de "DATABASE_URL"
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+# Before creating the engine, you check if the value of the DATABASE_URL is None, if it's you raise an error
+if DATABASE_URL is None:
+    raise ValueError("Variable 'DATABASE_URL' is not defined, check the .env URL.")
+
+# Pylance would raise an error here, because 'DATABASE_URL' could be None, but the previous 'raise' handles it (TYPE NARROWING)
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(bind=engine)
