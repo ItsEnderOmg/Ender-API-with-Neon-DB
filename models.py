e330f54630fe91612fef6cae5c8f14347abbc683
ItzEnderOmg <172705269+ItsEnderOmg@users.noreply.github.com>
2026-07-13 11:35:55 -0600

    Added the modules 'Mapped' and 'mapped_column' from sqlalchemy.orm in 'models.py' to prevent pydantic whimming, and also it's going to alert you to prevent typing errors, that's rlly nice.

--- models.py
+++ models.py
@@ -1,13 +1,15 @@
 #Here you create the colums to store information of the rows
 
 #You need to import the Datatypes that you're going to use in the columns
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Integer, String
+# You'll use this ones to prevent typing errors with pydantic later (you dont need Colum from sqlalchemy anymore)
+from sqlalchemy.orm import Mapped, mapped_column
 from database import Base
 
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    username = Column(String, unique=True)
-    password = Column(String)
-    email = Column(String, unique=True)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    username: Mapped[str] = mapped_column(String, unique=True)
+    password: Mapped[str] = mapped_column(String)
+    email: Mapped[str] = mapped_column(String, unique=True)
